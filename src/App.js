8874d867eede1f4e2c33f2a4bb62e9622f35320c
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-03-09 01:08:09 +0300

    s11g3-crud-filmler Esnek-görevler-kaldı

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
 import React, { useEffect, useState } from "react";
 
-import { Route, Switch, Redirect } from "react-router-dom";
-import MovieList from './components/MovieList';
-import Movie from './components/Movie';
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
+import MovieList from "./components/MovieList";
+import Movie from "./components/Movie";
+import EditMovieForm from "./components/EditMovieForm";
+import MovieHeader from "./components/MovieHeader";
+import AddMovieForm from "./components/AddMovieForm";
+import FavoriteMovieList from "./components/FavoriteMovieList";
 
-import MovieHeader from './components/MovieHeader';
-
-import FavoriteMovieList from './components/FavoriteMovieList';
-
-import axios from 'axios';
+import axios from "axios";
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
 
+  const { push } = useHistory;
   useEffect(() => {
-    axios.get('http://localhost:9000/api/movies')
-      .then(res => {
+    axios
+      .get("http://localhost:9000/api/movies")
+      .then((res) => {
         setMovies(res.data);
+        push("/movies");
       })
-      .catch(err => {
+      .catch((err) => {
         console.log(err);
       });
   }, []);
 
   const deleteMovie = (id) => {
-  }
+    axios
+      .delete(`http://localhost:9000/api/movies/${id}`)
+      .then((res) => {
+        setMovies(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
 
   const addToFavorites = (movie) => {
-
-  }
+    if (!favoriteMovies.find((mov) => mov === movie.id)) {
+      setFavoriteMovies([...favoriteMovies, movie]);
+    } else {
+      console.log("error");
+    }
+  };
 
   return (
     <div>
@@ -43,11 +56,18 @@ const App = (props) => {
           <FavoriteMovieList favoriteMovies={favoriteMovies} />
 
           <Switch>
+            <Route path="/movies/add">
+              <AddMovieForm setMovies={setMovies} />
+            </Route>
             <Route path="/movies/edit/:id">
+              <EditMovieForm setMovies={setMovies} />
             </Route>
 
             <Route path="/movies/:id">
-              <Movie />
+              <Movie
+                deleteMovie={deleteMovie}
+                addToFavorites={addToFavorites}
+              />
             </Route>
 
             <Route path="/movies">
@@ -64,6 +84,4 @@ const App = (props) => {
   );
 };
 
-
 export default App;
-
